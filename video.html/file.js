821ce93c363c0756d7feee378f3f0039d7ea1530
Mudassir  tahir <107237112+Mudassirgithub22@users.noreply.github.com>
2022-12-02 05:02:33 -0800

    first challenge

---
+++ video.html/file.js
@@ -0,0 +1,16 @@
+function click(){
+    
+    alert("03336937322")
+    
+}
+
+let sho=document.getElementById('sec');
+
+function enable()
+{
+    document.getElementById('sec').style.display='block';
+  sho=document.getElementById('sec').classList.add("open-section");
+
+}
+
+
